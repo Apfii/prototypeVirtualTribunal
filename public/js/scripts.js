3caf4a8f570d065f0eb21cdfeef65186ec3bae01
Mihai Ionescu <mihai.ionescu@cyberjustice.ca>
2018-04-06 16:13:06 -0400

    Moved scripts in js folder

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,7 +6,6 @@ var INDEX = 0;
 
 //
 var item_width = null;
-var interval = null;
 
 // Slider
 var slider1 = {
@@ -14,41 +13,56 @@ var slider1 = {
     x: 0,
     isHovered: false,
     isScrollForward: null
-}
-
-console.log(slider1)
+};
 
 window.onload = function() {
-    slider1.e = document.getElementById("test-slider-1");
+    setupSlider1();
+    setupSlider2();
+};
 
+// Slider 1
+// ======================================================================
+
+function setupSlider1() {
+
+    slider1.e = document.getElementById("test-slider-1");
     item_width = slider1.e.children[0].children[0].offsetWidth;
 
     var prev1 = slider1.e.parentElement.children[0];
     var next1 = slider1.e.parentElement.children[1];
+
+    var interval;
+    var translateSlider = function(slider) {
+        interval = setInterval(function(){
+            if(slider.isHovered) {
+                slider.x += slider.isScrollForward ? 5 : -5;
+                if(slider.x < 0) slider.x = 0;
+                else if(slider.x > item_width*(TOTAL_ITEMS-SLIDER_SIZE)-item_width*0.33) slider.x = item_width*(TOTAL_ITEMS-SLIDER_SIZE)-item_width*0.33;
+                slider.e.style = "transform: translateX(-"+slider.x+"px)";
+            } else clearInterval(interval);
+        }, 10)
+    }
     
     prev1.onmouseenter = function() {
         slider1.isHovered = true;
         slider1.isScrollForward = false;
         translateSlider(slider1);
     };
+
     next1.onmouseenter = function() {
         slider1.isHovered = true;
         slider1.isScrollForward = true;
         translateSlider(slider1);
     };
+
     next1.onmouseleave = prev1.onmouseleave = function() {
         slider1.isHovered = false;
     };
-};
+}
 
+// Slider 2
+// ======================================================================
 
-var translateSlider = function(slider) {
-    interval = setInterval(function(){
-        if(slider.isHovered) {
-            slider.x += slider.isScrollForward ? 5 : -5;
-            if(slider.x < 0) slider.x = 0;
-            else if(slider.x > item_width*(TOTAL_ITEMS-SLIDER_SIZE)-item_width*0.33) slider.x = item_width*(TOTAL_ITEMS-SLIDER_SIZE)-item_width*0.33;
-            slider.e.style = "transform: translateX(-"+slider.x+"px)";
-        } else clearInterval(interval);
-    }, 10)
+function setupSlider2() {
+
 }
